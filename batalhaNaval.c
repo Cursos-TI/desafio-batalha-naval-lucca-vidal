422242374a73c7b1aaa9bebe8e42408d43154d83
lucca-vidal <72407260+lucca-vidal@users.noreply.github.com>
2026-06-10 04:06:01 +0000

    colocado 2 navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,10 @@ int main() {
 
     //declaração das matrizes e das variáveis
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; //vetor para indicar a coluna do tabuleiro
-    int navio_horizontal[3] = {3, 3, 3};//vetor para colocar o navio na horientação horizontal
-    int navio_vertical[3] = {3, 3, 3};//vetor para colocar o navio na horientação vertical
+    int navio_horizontal[3] = {3, 3, 3};//vetor para colocar o navio na horizontal
+    int navio_vertical[3] = {3, 3, 3};//vetor para colocar o navio na vertical
+    int navio_diagonal1[3] = {3, 3, 3};//vetor para colocar o primeiro navio na diagonal 
+    int navio_diagonal2[3] = {3, 3, 3};//vetor para colocar o segundo navio na diagonal 
     int tabuleiro[10][10] = {0};//matriz para fazer o tabuleiro
     int linha2;//variáveis para printar o tabuleiro 
     int coluna2;
@@ -18,6 +20,13 @@ int main() {
     int linha_v = 5;
     int coluna_v = 8;
 
+    //variáveis para mostrar os navios na diagonal
+    int linha_diagonal1 = 0;
+    int coluna_diagonall = 2;
+
+    int linha_diagonal2 = 5;
+    int coluna_diagonal2 = 3;
+
     // Posiciona o navio horizontal
     for (int i = 0; i < 3; i++) {
         tabuleiro[linha_h][coluna_h + i] = navio_horizontal[i];
@@ -27,6 +36,19 @@ int main() {
     for (int i = 0; i < 3; i++) {
         tabuleiro[linha_v + i][coluna_v] = navio_vertical[i];
     }
+
+    //posiciona o navio na diagonal
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro[linha_diagonal1 + i][coluna_diagonall - i] = navio_diagonal1[i];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro[linha_diagonal2 - i][coluna_diagonal2 - i] = navio_diagonal2[i];
+    }
+    
+    
 
     printf("----TABULEIRO BATALHA NAVAL----\n");
     printf("   ");
